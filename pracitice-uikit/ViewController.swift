5cbd28bac934b407b03152b34649294283abd2f3
Yersultan <ye.zhetibayev@gmail.com>
2025-07-04 01:50:08 +0500

    Initial Commit

---
+++ pracitice-uikit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pracitice-uikit
+//
+//  Created by Ersultan on 04.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
